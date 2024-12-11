d8b7e75baadc4c09f045621fb99a0ba74073edb4
Curlytame <164329092+Curlytame@users.noreply.github.com>
2024-12-11 15:52:28 +0800

    Buying system

--- Assets/Script/FloorScript.cs
+++ Assets/Script/FloorScript.cs
@@ -4,59 +4,69 @@ using UnityEngine;
 
 public class FloorScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private MoneySystem moneySystem; // Reference to the MoneySystem script
+
     void Start()
     {
-        
+        // Find the MoneySystem in the scene
+        moneySystem = FindObjectOfType<MoneySystem>();
+
+        if (moneySystem == null)
+        {
+            Debug.LogError("MoneySystem not found in the scene!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnCollisionEnter2D(Collision2D collision)
     {
+        // If the object has already collided, do nothing
+        if (collision.gameObject.CompareTag("Processed"))
+            return;
+
+        // Mark the object as processed by giving it a "Processed" tag
+        collision.gameObject.tag = "Processed";
+
         if (collision.gameObject.CompareTag("Bran"))
         {
             Destroy(collision.gameObject);
         }
-        if (collision.gameObject.CompareTag("BrownRice"))
+        else if (collision.gameObject.CompareTag("BrownRice"))
         {
             Destroy(collision.gameObject);
         }
-        if (collision.gameObject.CompareTag("CrackRice"))
+        else if (collision.gameObject.CompareTag("CrackRice"))
         {
             Destroy(collision.gameObject);
         }
-        if (collision.gameObject.CompareTag("Husk"))
+        else if (collision.gameObject.CompareTag("Husk"))
         {
             Destroy(collision.gameObject);
         }
-        if (collision.gameObject.CompareTag("Paddy"))
+        else if (collision.gameObject.CompareTag("Paddy"))
         {
             Destroy(collision.gameObject);
         }
-        if (collision.gameObject.CompareTag("RiceFlour"))
+        else if (collision.gameObject.CompareTag("RiceFlour"))
         {
             Destroy(collision.gameObject);
         }
-        if (collision.gameObject.CompareTag("Spiklet"))
+        else if (collision.gameObject.CompareTag("Spiklet"))
+        {
+            moneySystem.AddMoney(5);
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Stalk"))
         {
             Destroy(collision.gameObject);
         }
-
-        if (collision.gameObject.CompareTag("Stalk"))
+        else if (collision.gameObject.CompareTag("StandardRice"))
         {
             Destroy(collision.gameObject);
         }
-        if (collision.gameObject.CompareTag("StandardRice"))
-        {
-            Destroy(collision.gameObject);
-        }
-        if (collision.gameObject.CompareTag("PremiumRice"))
+        else if (collision.gameObject.CompareTag("PremiumRice"))
         {
             Destroy(collision.gameObject);
         }
     }
 }
+
