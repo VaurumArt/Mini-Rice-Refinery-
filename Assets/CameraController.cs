c287760b5d8dba3309f9ca719a7a1059ba1667e0
VaurumArt <164336259+VaurumArt@users.noreply.github.com>
2024-12-11 18:19:45 +0800

    new fixes

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -9,8 +9,8 @@ public class CameraController : MonoBehaviour
     public float edgeThickness = 10f;    // Thickness of the screen edge to trigger movement
 
     [Header("Camera Bounds")]
-    public float minY = 0f;              // Minimum Y boundary for the camera
-    public float maxY = 50f;             // Maximum Y boundary for the camera
+    public float minY = 70f; //50 //last machine -10              // Minimum Y boundary for the camera
+    public float maxY = 70f;             // Maximum Y boundary for the camera
 
     void Update()
     {
