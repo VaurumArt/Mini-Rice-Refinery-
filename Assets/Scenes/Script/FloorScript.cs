92364018302b08d410e3adee7fcdc6a581fe01e3
Curlytame <164329092+Curlytame@users.noreply.github.com>
2024-12-11 22:36:32 +0800

    Floor

--- Assets/Scenes/Script/FloorScript.cs
+++ Assets/Scenes/Script/FloorScript.cs
@@ -6,6 +6,19 @@ public class FloorScript : MonoBehaviour
 {
     private MoneySystem moneySystem; // Reference to the MoneySystem script
 
+    // Define positions for the floor based on the enabled machine
+    public Vector3 defaultPosition; // Default position of the floor
+    public Vector3 machine1Position; // Position when Machine 1 is enabled
+    public Vector3 machine2Position; // Position when Machine 2 is enabled
+    public Vector3 machine3Position; // Position when Machine 3 is enabled
+    public Vector3 machine4Position; // Position when Machine 3 is enabled
+
+    // References to the machines
+    public GameObject machine1;
+    public GameObject machine2;
+    public GameObject machine3;
+    public GameObject machine4;
+
     void Start()
     {
         // Find the MoneySystem in the scene
@@ -14,6 +27,39 @@ public class FloorScript : MonoBehaviour
         if (moneySystem == null)
         {
             Debug.LogError("MoneySystem not found in the scene!");
+        }
+
+        // Set the floor to its default position at the start
+        transform.position = defaultPosition;
+    }
+
+    public void FixedUpdate()
+    {
+        UpdateFloorPosition();
+    }
+
+    void UpdateFloorPosition()
+    {
+        // Check the state of the machines and prioritize the most expensive/last machine
+        if (machine4.activeSelf)
+        {
+            transform.position = machine4Position;
+        }
+        else if (machine3.activeSelf)
+        {
+            transform.position = machine3Position;
+        }
+        else if (machine2.activeSelf)
+        {
+            transform.position = machine2Position;
+        }
+        else if (machine1.activeSelf)
+        {
+            transform.position = machine1Position;
+        }
+        else
+        {
+            transform.position = defaultPosition;
         }
     }
 
@@ -28,43 +74,52 @@ public class FloorScript : MonoBehaviour
 
         if (collision.gameObject.CompareTag("Bran"))
         {
+            moneySystem.AddMoney(5);
             Destroy(collision.gameObject);
         }
         else if (collision.gameObject.CompareTag("BrownRice"))
         {
+            moneySystem.AddMoney(5);
             Destroy(collision.gameObject);
         }
         else if (collision.gameObject.CompareTag("CrackRice"))
         {
+            moneySystem.AddMoney(5);
             Destroy(collision.gameObject);
         }
         else if (collision.gameObject.CompareTag("Husk"))
         {
+            moneySystem.AddMoney(5);
             Destroy(collision.gameObject);
         }
         else if (collision.gameObject.CompareTag("Paddy"))
         {
+            moneySystem.AddMoney(5);
             Destroy(collision.gameObject);
         }
         else if (collision.gameObject.CompareTag("RiceFlour"))
         {
+            moneySystem.AddMoney(5);
             Destroy(collision.gameObject);
         }
-       // else if (collision.gameObject.CompareTag("Spiklet"))
-      //  {
-      //   moneySystem.AddMoney(5);
-      //     Destroy(collision.gameObject);
-      // }
+        else if (collision.gameObject.CompareTag("Spiklet"))
+        {
+            moneySystem.AddMoney(5);
+            Destroy(collision.gameObject);
+        }
         else if (collision.gameObject.CompareTag("Stalk"))
         {
+            moneySystem.AddMoney(5);
             Destroy(collision.gameObject);
         }
         else if (collision.gameObject.CompareTag("StandardRice"))
         {
+            moneySystem.AddMoney(5);
             Destroy(collision.gameObject);
         }
         else if (collision.gameObject.CompareTag("PremiumRice"))
         {
+            moneySystem.AddMoney(5);
             Destroy(collision.gameObject);
         }
     }
